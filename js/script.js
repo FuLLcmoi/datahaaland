aadb9ba78f6c8b13ac95063c6eebb3915495d764
FuLL <139165917+FuLLcmoi@users.noreply.github.com>
2023-07-30 22:16:55 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -17,11 +17,51 @@ const performanceData = {
     { competition: "Ligue des Champions", Matchs: 0, goals: 0, assists: 0, minutes: 0, rating: 0 },
   ],
   "2022-2023": [
-    { competition: "Premier League", Matchs: 35, goals: 36, assists: 8, minutes: 2779, rating: 7.36 },
-    { competition: "Ligue des Champions", Matchs: 11, goals: 12, assists: 1, minutes: 848, rating: 7.41 },
-    { competition: "FA Cup", Matchs: 4, goals: 3, assists: 0, minutes: 311, rating: 7.20 },
-    { competition: "League Cup", Matchs: 2, goals: 1, assists: 0, minutes: 107, rating: 6.75 },
-    { competition: "Community Shield", Matchs: 1, goals: 0, assists: 0, minutes: 90, rating: 6.20 },
+    { competition: "Premier League", Matchs: 35, goals: 36, assists: 8,},
+    { competition: "Ligue des Champions", Matchs: 11, goals: 12, assists: 1, },
+    { competition: "FA Cup", Matchs: 4, goals: 3, assists: 0,},
+    { competition: "League Cup", Matchs: 2, goals: 1, assists: 0,},
+    { competition: "Community Shield", Matchs: 1, goals: 0, assists: 0,},
+  ],
+  "2021-2022": [
+    { competition: "Bundesliga", Matchs: 24, goals: 22, assists: 7, },
+    { competition: "Ligue des Champions", Matchs: 3, goals: 3, assists: 0, },
+    { competition: "WC Qualification Europe", Matchs: 6, goals: 5, assists: 0,},
+    { competition: "DFB Pokal", Matchs: 2, goals: 4, assists: 0, },
+    { competition: "DFL Super Cup", Matchs: 1, goals: 0, assists: 0,},
+  ],
+  "2020-2021": [
+    { competition: "Bundesliga", Matchs: 28, goals: 27, assists: 6,},
+    { competition: "Ligue des Champions", Matchs: 8, goals: 10, assists: 2, },
+    { competition: "DFB Pokal", Matchs: 4, goals: 3, assists: 1, },
+    { competition: "DFL Super Cup", Matchs: 1, goals: 1, assists: 1, },
+    { competition: "UEFA Nations League", Matchs: 4, goals: 6, assists: 1, },
+  ],
+  "2019-2020": [
+    { competition: "Bundesliga", Matchs: 14, goals: 16, assists: 4,},
+    { competition: "Ligue des Champions", Matchs: 8, goals: 10, assists: 2, },
+    { competition: "DFB Pokal", Matchs: 4, goals: 3, assists: 1, },
+    { competition: "Austrian Cup", Matchs: 1, goals: 4, assists: 0,},
+    { competition: "Éliminatoires Euro", Matchs: 3, goals: 0, assists: 0, },
+  ],
+  "2018-2019": [
+    { competition: "Bundesliga", Matchs: 2, goals: 1, assists: 0, },
+    { competition: "Europa Ligue", Matchs: 6, goals: 4, assists: 0, },
+    { competition: "OFB Cup", Matchs: 2, goals: 0, assists: 0, },
+    { competition: "World Cup U20", Matchs: 3, goals: 9, assists: 0, },
+    { competition: "Euro U21", Matchs: 3, goals: 0, assists: 1, },
+  ],
+  "2017-2018": [
+    { competition: "Eliteserien", Matchs: 25, goals: 12, assists: 4,},
+    { competition: "Youth League", Matchs: 1, goals: 0, assists: 0,},
+    { competition: "Euro U19", Matchs: 9, goals: 10, assists: 2, },
+  ],
+  "2016-2017": [
+    { competition: "Eliteserien", Matchs: 14, goals: 2, assists: 1, },
+    { competition: "Norvège Cup", Matchs: 6, goals: 2, assists: 0,},
+  ],
+  "2015-2016": [
+    { competition: "D1 Norvègienne", Matchs: 16, goals: 0, assists: 0,},
   ],
 };
 
